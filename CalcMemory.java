a8f9ffadbbdd9c6e58a44122a49a6674b6e43222
PiotrJusz <pepiklg@gmail.com>
2023-04-28 19:09:43 +0100

    Added CalcMemory class. It make memory for calculator and store one value. Addition class now working wich CalcMemory class.

---
+++ CalcMemory.java
@@ -0,0 +1,29 @@
+public class CalcMemory{
+    //variable stored current number which user ask to remeber
+    double memory = 0;
+
+    //constructor, n'num' is number given by user
+    public CalcMemory(double num){
+        this.memory = num;
+    }
+
+    public void setMemory(double num){
+        this.memory = num;
+    }
+
+    //delete memory (set as 0)
+    public void resetMemory(){
+        this.memory = 0;
+    }
+
+    //get number which is already stored in variable 'memory' 
+    public double getMemory(){
+        return this.memory;
+    }
+
+    
+
+
+
+
+}
